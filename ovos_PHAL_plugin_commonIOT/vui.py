989ab10147849fbe1aa8349141a3e8451909ebf1
builderjer <builderjer@gmail.com>
2023-07-31 06:46:05 -0600

    fixed bus.emit messages...I think

--- ovos_PHAL_plugin_commonIOT/vui.py
+++ ovos_PHAL_plugin_commonIOT/vui.py
@@ -1,5 +1,6 @@
 from ovos_workshop.app import OVOSAbstractApplication
 from ovos_workshop.decorators import intent_handler
+from ovos_bus_client import Message
 from ovos_utils.log import LOG
 
 class IOTVoiceInterface(OVOSAbstractApplication):
@@ -9,7 +10,8 @@ class IOTVoiceInterface(OVOSAbstractApplication):
 
     @intent_handler("light.get.brightness.intent")
     def light_get_brightness_intent(self, message):
-        response = self.bus.emit("ovos.iot.device.get.brightness", message)
+        device = message.data.get("device")
+        response = self.bus.emit(Message("ovos.iot.device.get.brightness", data={"device": device}))
         if not response:
             self.speak_dialog("device.not.found.dialog", data={"device": message.data.device.get("name", "unknown")})
         else:
@@ -17,53 +19,66 @@ class IOTVoiceInterface(OVOSAbstractApplication):
                                                                        "brightness": response})
     @intent_handler("light.set.brightness.intent")
     def light_set_brightness_intent(self, message):
-        response = self.bus.emit("ovos.iot.device.set.brightness", message)
+        device = message.data.get("device")
+        brightness = message.data.get("brightness")
+        response = self.bus.emit(Message("ovos.iot.device.set.brightness", data={"device": device,
+                                                                                 "brightness": brightness}))
         if not response:
-            self.speak_dialog("device.not.found.dialog", data="device": message.data.device.get("name", "unknown"))
+            self.speak_dialog("device.not.found.dialog", data="device": device.get("name", "unknown"))
         elif isinstance(response, Exception):
-            self.speak_dialog("light.set.brightness.error.dialog", data={"device": message.data.device.get("name", "unknown")})
+            self.speak_dialog("light.set.brightness.error.dialog", data={"device": device.get("name", "unknown")})
         else:
-            self.speak_dialog("light.set.brightness.dialog", data={"device": message.data.device.get("name"),
+            self.speak_dialog("light.set.brightness.dialog", data={"device": device.get("name"),
                                                                        "brightness": response})
 
     @intent_handler("light.decrease.brightness.intent")
     def light_decrease_brightness_intent(self, message):
-        response = self.bus.emit("ovos.iot.device.decrease.brightness", message)
+        device = message.get("device")
+        amount = message.get("amount", 10)
+        response = self.bus.emit(Message("ovos.iot.device.decrease.brightness", data={"device": device,
+                                                                                      "amount": amount}))
         if not response:
-            self.speak_dialog("device.not.found.dialog", data="device": message.data.device.get("name", "unknown"))
+            self.speak_dialog("device.not.found.dialog", data="device": device.get("name", "unknown"))
         elif isinstance(response, Exception):
-            self.speak_dialog("light.set.brightness.error.dialog", data={"device": message.data.device.get("name", "unknown")})
+            self.speak_dialog("light.set.brightness.error.dialog", data={"device": device.get("name", "unknown")})
         else:
-            self.speak_dialog("light.set.brightness.dialog", data={"device": message.data.device.get("name"),
+            self.speak_dialog("light.set.brightness.dialog", data={"device": device.get("name"),
                                                                        "brightness": response})
 
     @intent_handler("light.increase.brightness.intent")
     def light_increase_brightness_intent(self, message):
-        response = self.bus.emit("ovos.iot.device.increase.brightness", message)
+        device = message.get("device")
+        amount = message.get("amount", 10)
+        response = self.bus.emit(Message("ovos.iot.device.increase.brightness", data={"device": device,
+                                                                                      "amount": amount}))
         if not response:
-            self.speak_dialog("device.not.found.dialog", data="device": message.data.device.get("name", "unknown"))
+            self.speak_dialog("device.not.found.dialog", data="device": device.get("name", "unknown"))
         elif isinstance(response, Exception):
-            self.speak_dialog("light.set.brightness.error.dialog", data={"device": message.data.device.get("name", "unknown")})
+            self.speak_dialog("light.set.brightness.error.dialog", data={"device": device.get("name", "unknown")})
         else:
-            self.speak_dialog("light.set.brightness.dialog", data={"device": message.data.device.get("name"),
+            self.speak_dialog("light.set.brightness.dialog", data={"device": device.get("name"),
                                                                        "brightness": response})
 
     @intent_handler("light.get.color.intent")
     def light_get_color_intent(self, message):
-        response = self.bus.emit("ovos.iot.device.get.color", message)
+        device = message.get("device")
+        response = self.bus.emit(Message("ovos.iot.device.get.color", data={"device": device}))
         if not response:
-            self.speak_dialog("device.not.found.dialog", data={"device": message.data.device.get("name", "unknown")})
+            self.speak_dialog("device.not.found.dialog", data={"device": device.get("name", "unknown")})
         else:
-            self.speak_dialog("light.current.color.dialog", data={"device": message.data.device.get("name"), "color": response})
+            self.speak_dialog("light.current.color.dialog", data={"device": device.get("name"), "color": response})
 
     @intent_handler("light.set.color.intent")
     def light_set_color_intent(self, message):
-        response = self.bus.emit("ovos.iot.device.set.color", message)
+        device = message.get("device")
+        color = message.get("color")
+        response = self.bus.emit(Message("ovos.iot.device.set.color", data={"device": device,
+                                                                            "color": color}))
         if not response:
-            self.speak_dialog("device.not.found.dialog", data="device": message.data.device.get("name", "unknown"))
+            self.speak_dialog("device.not.found.dialog", data="device": device.get("name", "unknown"))
         elif isinstance(response, Exception):
-            self.speak_dialog("light.set.color.error.dialog", data={"device": message.data.device.get("name", "unknown")})
+            self.speak_dialog("light.set.color.error.dialog", data={"device": device.get("name", "unknown")})
         else:
-            self.speak_dialog("light.set.color.dialog", data={"device": message.data.device.get("name"),
+            self.speak_dialog("light.set.color.dialog", data={"device": device.get("name"),
                                                                        "color": response})
 
